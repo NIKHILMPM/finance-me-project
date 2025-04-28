b0be38ad74fed29abbeca07e9623b3ab4f62ae61
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-28 21:31:11 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "finance_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"  
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "FinanceMe-Server"
+  }
+
+  vpc_security_group_ids = [aws_security_group.finance_sg.id]
+}
+
+resource "aws_security_group" "finance_sg" {
+  name        = "finance_sg"
+  description = "Allow SSH and 8081"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
