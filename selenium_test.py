b716cde9ad66ebaff06e8415685717f6cf503ffc
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-28 23:32:30 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
+
+# Set options for headless mode (Jenkins server usually has no GUI)
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+# Launch browser
+driver = webdriver.Chrome(options=chrome_options)
 
 # URL of your deployed app
 url = "http://54.167.8.65:8081"
-
-# Launch browser
-driver = webdriver.Chrome()
 
 # Open the app URL
 driver.get(url)
