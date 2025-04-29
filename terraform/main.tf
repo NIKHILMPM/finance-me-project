664791ed52748a1c671f3db56f061e28d8eef2e1
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-29 20:02:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,16 +7,6 @@ resource "aws_instance" "finance_ec2" {
   instance_type = "t2.micro"
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y docker.io
-              systemctl start docker
-              systemctl enable docker
-              docker pull ramachandrampm/financeme-image:latest
-              docker run -d --name financeme-container -p 8081:8081 ramachandrampm/financeme-image:latest
-              EOF
-
   tags = {
     Name = "FinanceMe-Server"
   }
@@ -50,7 +40,6 @@ resource "aws_security_group" "finance_sg" {
   }
 }
 
-# New added Terraform output
 output "ec2_public_ip" {
   value = aws_instance.finance_ec2.public_ip
 }
