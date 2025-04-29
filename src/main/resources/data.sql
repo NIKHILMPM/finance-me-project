d3306d272fce6218d8f97a803034ba477e0dcc9b
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-29 22:11:26 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
-INSERT INTO account (account_number, account_name, account_type, balance) VALUES (1, 'John Doe', 'Saving', 10000.00);
+INSERT INTO account (account_number, account_name, account_type, account_balance)
+VALUES (1, 'John Doe', 'Saving', 10000.0);
