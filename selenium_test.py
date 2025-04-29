1d686e606ca3f9401c48cba21c7e003b5ae3cc0c
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-29 12:47:29 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,20 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import tempfile
+
+# Create a truly fresh temp dir
+user_data_dir = tempfile.mkdtemp()
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--user-data-dir=/tmp/selenium")
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome(options=chrome_options)
 
-url = "http://54.167.8.65:8081"
+url = "http://34.229.221.148:8081"
 
 driver.get(url)
+
 time.sleep(5)
 
 driver.save_screenshot("homepage.png")
 
 driver.quit()
+
