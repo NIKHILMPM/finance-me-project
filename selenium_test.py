4c3da7aed24152f7b5117efe2fa5060953cb74c2
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-29 12:43:56 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -6,6 +6,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--user-data-dir=/tmp/selenium")
 
 driver = webdriver.Chrome(options=chrome_options)
 
