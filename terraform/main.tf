51be337a7b25bc974a5eb46fccfeac439e7cd35b
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-29 16:35:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "finance_ec2" {
-  ami           = "ami-0e449927258d45bc4"
+  ami           = "ami-0f9de6e2d2f067fca"
   instance_type = "t2.micro"
   associate_public_ip_address = true
 
