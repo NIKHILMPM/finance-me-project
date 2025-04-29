9e458daa8e4f8cde17ea05ec143592e415419608
NIKHILMPM <157490189+NIKHILMPM@users.noreply.github.com>
2025-04-29 14:43:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "finance_ec2" {
   ami           = "ami-0e449927258d45bc4"  
   instance_type = "t2.micro"
   associate_public_ip_address = true
+  key_name = "jjk"   
 
   tags = {
     Name = "FinanceMe-Server"
